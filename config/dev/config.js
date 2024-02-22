5aee62072797e6b566d1761f0d6ffd8e03799f82
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2024-02-22 17:09:03 +0000

    chore: javascript lint

--- config/dev/config.js
+++ config/dev/config.js
@@ -1,3 +1,7 @@
 // overriding configuration for the intellireading web app
-var metaguideEpubUrl = 'http://localhost:80/metaguiding/epub/transform/submit'
-var metaguideXtmlUrl = 'http://localhost:80/metaguiding/xhtml/transform'
+const metaguideEpubUrl = 'http://localhost:80/metaguiding/epub/transform/submit'
+const metaguideXtmlUrl = 'http://localhost:80/metaguiding/xhtml/transform'
+
+// output values to the console
+console.log(metaguideEpubUrl)
+console.log(metaguideXtmlUrl)
--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,7 @@
 // default configuration for the intellireading web app
-var metaguideEpubUrl = 'https://api.intellireading.com/metaguiding/epub/transform/submit'
-var metaguideXtmlUrl = 'https://api.intellireading.com/metaguiding/xhtml/transform'
+const metaguideEpubUrl = 'https://api.intellireading.com/metaguiding/epub/transform/submit'
+const metaguideXtmlUrl = 'https://api.intellireading.com/metaguiding/xhtml/transform'
+
+// output values to the console
+console.log(metaguideEpubUrl)
+console.log(metaguideXtmlUrl)
